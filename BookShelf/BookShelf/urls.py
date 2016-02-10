9d3803a23762f0e521cd00bf34cd583063b1a9fe
Krzysztof Studniarek <krzysztof.studniarek@mbank.pl>
2016-02-10 13:24:04 +0100

    Initial commit

---
+++ BookShelf/BookShelf/urls.py
@@ -0,0 +1,25 @@
+﻿"""
+Definition of urls for BookShelf.
+"""
+
+from datetime import datetime
+from django.conf.urls import patterns, url
+
+# Uncomment the next lines to enable the admin:
+# from django.conf.urls import include
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'app.views.home', name='home'),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^register/$', 'app.views.register', name='register'),
+    url(r'^register/success/$', 'app.views.register_success', name='register_success'),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
---
+++ BookShelf/app/views.py
@@ -0,0 +1,51 @@
+﻿"""
+Definition of views.
+"""
+
+from django.shortcuts import render, render_to_response
+from django.http import HttpRequest, HttpResponseRedirect
+from django.template import RequestContext
+from datetime import datetime
+from app.forms import *
+from django.views.decorators.csrf import csrf_protect
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Home Page',
+            'year':datetime.now().year
+        },
+        context_instance = RequestContext(request)
+    )
+
+@csrf_protect
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password1'],
+            email=form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/register/success/')
+    else:
+        form = RegistrationForm()
+    variables = RequestContext(request, {
+    'form': form
+    })
+ 
+    return render_to_response(
+    'registration/register.html',
+    variables,
+    )
+ 
+def register_success(request):
+    return render_to_response(
+    'registration/success.html',
+    )
